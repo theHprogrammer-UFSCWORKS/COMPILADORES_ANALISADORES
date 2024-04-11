d72222ad7644b59a3e27891a85be1b09e5df834f
theHprogrammer <helder.h.silva@grad.ufsc.br>
2024-04-11 12:13:44 -0300

    feats: Reduzido quantidade de testes e implementado testes unitários.

--- tests/test_lexer_pytest.py
+++ tests/test_lexer_pytest.py
@@ -9,13 +9,13 @@ def test_case0():
     with open(test_file_path, 'r', encoding='utf-8') as file:
         code = file.read()
         lexer.input(code)
-
+    
     # Gerar a lista de tokens
     tokens = list(lexer.tokenize())
-
+    
     # Realizar as verificações
     assert tokens[0].type == 'INT'        # int
-    assert tokens[1].type == 'ID'         # main
+    assert tokens[1].type == 'MAIN'       # main
     assert tokens[2].type == 'LPAREN'     # (
     assert tokens[3].type == 'RPAREN'     # )
     assert tokens[4].type == 'LBRACE'     # {
@@ -39,3 +39,169 @@ def test_case0():
     assert tokens[22].type == 'SEMICOLON' # ;
     assert tokens[23].type == 'RBRACE'    # }
     assert len(tokens) == 24
+
+def test_case1():
+    lexer = Lexer()
+    test_file_path = TEST_FILES_PATH + 'test_case1.c'
+    with open(test_file_path, 'r', encoding='utf-8') as file:
+        code = file.read()
+        lexer.input(code)
+        
+    # A lista de tipos de tokens esperados para esse caso de teste
+    expected_tokens_types = [
+        'INT', 'MAIN', 'LPAREN', 'RPAREN', 'LBRACE',
+        'INT', 'ID', 'EQUALS', 'INTEGER', 'SEMICOLON',
+        'INT', 'ID', 'EQUALS', 'INTEGER', 'SEMICOLON',
+        'IF', 'LPAREN', 'ID', 'LT', 'INTEGER', 'RPAREN', 'LBRACE',
+        'ID', 'EQUALS', 'INTEGER', 'SEMICOLON', 'RBRACE',
+        'ELSE', 'LBRACE', 'IF', 'LPAREN', 'ID', 'LE', 'INTEGER', 'RPAREN', 'LBRACE',
+        'ID', 'EQUALS', 'INTEGER', 'SEMICOLON', 'RBRACE', 'ELSE', 'LBRACE',
+        'ID', 'EQUALS', 'INTEGER', 'SEMICOLON', 'RBRACE', 'RBRACE', 'RBRACE'
+    ]
+    
+    # Gerar a lista de tokens
+    tokens = list(lexer.tokenize())
+    
+    # Realizar as verificações
+    for tipo_esperado, token in zip(expected_tokens_types, tokens):
+        assert token.type == tipo_esperado, f"Esperado {tipo_esperado}, obtido {token.type if token else 'None'}"
+    
+    # Verifica se todos os tokens esperados foram encontrados
+    assert len(tokens) == len(expected_tokens_types), f"Número de tokens esperados {len(expected_tokens_types)}, mas obtido {len(tokens)}"
+    
+    # Após a lista de tokens esperados, não deve haver mais tokens
+    assert lexer.token() is None, "Tokens extras encontrados após os esperados."
+
+def test_case2():
+    lexer = Lexer()
+    test_file_path = TEST_FILES_PATH + 'test_case2.c'
+    with open(test_file_path, 'r', encoding='utf-8') as file:
+        code = file.read()
+        lexer.input(code)
+        
+            # A lista de tipos de tokens esperados para esse caso de teste
+    expected_tokens_types = [
+        'INT', 'MAIN', 'LPAREN', 'RPAREN', 'LBRACE',
+        'INT', 'ID', 'EQUALS', 'INTEGER', 'SEMICOLON',
+        'FLOAT', 'ID', 'EQUALS', 'FLOAT_N', 'SEMICOLON',
+        'WHILE', 'LPAREN', 'ID', 'GT', 'INTEGER', 'RPAREN', 'LBRACE',
+        'ID', 'LPAREN', 'RPAREN', 'SEMICOLON',
+        'RBRACE', 'RBRACE', 'SEMICOLON'
+    ]
+    
+    # Gerar a lista de tokens
+    tokens = list(lexer.tokenize())
+    
+    # Realizar as verificações
+    for expected_type, token in zip(expected_tokens_types, tokens):
+        assert token.type == expected_type, f"Esperado {expected_type}, obtido {token.type if token else 'Nenhum'}"
+    
+    # Verifica se todos os tokens esperados foram encontrados
+    assert len(tokens) == len(expected_tokens_types), f"Número de tokens esperado {len(expected_tokens_types)}, mas obtido {len(tokens)}"
+    
+    # Após a lista de tokens esperados, não deve haver mais tokens
+    assert lexer.token() is None, "Tokens extras encontrados após os esperados."
+
+def test_case3():
+    lexer = Lexer()
+    test_file_path = TEST_FILES_PATH + 'test_case3.c'
+    with open(test_file_path, 'r', encoding='utf-8') as file:
+        code = file.read()
+        lexer.input(code)
+    
+    expected_tokens_types = [
+        'INT', 'MAIN', 'LPAREN', 'RPAREN', 'LBRACE',
+        'INT', 'ID', 'EQUALS', 'INTEGER', 'SEMICOLON',
+        'INT', 'ID', 'EQUALS', 'INTEGER', 'SEMICOLON',
+        'WHILE', 'LPAREN', 'ID', 'LE', 'INTEGER', 'RPAREN', 'LBRACE',
+        'ID', 'EQUALS', 'ID', 'PLUS', 'INTEGER', 'SEMICOLON',
+        'RBRACE', 'RBRACE'
+    ]
+    
+    tokens = list(lexer.tokenize())
+    
+    # Realizar as verificações
+    for expected_type, token in zip(expected_tokens_types, tokens):
+        assert token.type == expected_type, f"Esperado {expected_type}, obtido {token.type if token else 'Nenhum'}"
+    
+    # Verifica se todos os tokens esperados foram encontrados
+    assert len(tokens) == len(expected_tokens_types), f"Número de tokens esperado {len(expected_tokens_types)}, mas obtido {len(tokens)}"
+    
+    # Após a lista de tokens esperados, não deve haver mais tokens
+    assert lexer.token() is None, "Tokens extras encontrados após os esperados."
+
+def test_case4():
+    lexer = Lexer()
+    test_file_path = TEST_FILES_PATH + 'test_case4.c'
+    with open(test_file_path, 'r', encoding='utf-8') as file:
+        code = file.read()
+        lexer.input(code)
+    
+    expected_tokens_types = [
+        'INCLUDE', 'LIBRARY',
+        'VOID', 'ID', 'LPAREN', 'RPAREN', 'LBRACE',
+        'ID', 'LPAREN', 'STRING', 'RPAREN', 'SEMICOLON', 'RBRACE',
+        'INT', 'ID', 'LPAREN', 'INT', 'ID', 'COMMA', 'INT', 'ID', 'RPAREN', 'LBRACE',
+        'RETURN', 'ID', 'PLUS', 'ID', 'SEMICOLON', 'RBRACE',
+        'INT', 'MAIN', 'LPAREN', 'RPAREN', 'LBRACE',
+        'INT', 'ID', 'EQUALS', 'ID', 'LPAREN', 'INTEGER', 'COMMA', 'INTEGER', 'RPAREN', 'SEMICOLON',
+        'IF', 'LPAREN', 'ID', 'GT', 'INTEGER', 'RPAREN', 'LBRACE',
+        'ID', 'LPAREN', 'RPAREN', 'SEMICOLON', 'RBRACE',
+        'RETURN', 'INTEGER', 'SEMICOLON', 'RBRACE'
+    ]
+    
+    tokens = list(lexer.tokenize())
+    
+    # Realizar as verificações
+    for expected_type, token in zip(expected_tokens_types, tokens):
+        assert token.type == expected_type, f"Esperado {expected_type}, obtido {token.type if token else 'Nenhum'}"
+    
+    # Verifica se todos os tokens esperados foram encontrados
+    assert len(tokens) == len(expected_tokens_types), f"Número de tokens esperado {len(expected_tokens_types)}, mas obtido {len(tokens)}"
+    
+    # Após a lista de tokens esperados, não deve haver mais tokens
+    assert lexer.token() is None, "Tokens extras encontrados após os esperados."
+
+def test_case5():
+    lexer = Lexer()
+    test_file_path = TEST_FILES_PATH + 'test_case5.c'
+    with open(test_file_path, 'r', encoding='utf-8') as file:
+        code = file.read()
+        lexer.input(code)
+    
+    expected_tokens_types = [
+        'INCLUDE', 'LIBRARY',
+        'INCLUDE', 'LIBRARY',
+        'DEFINE', 'ID', 'INTEGER', 
+        'TYPEDEF', 'STRUCT', 'LBRACE', 
+        'FLOAT', 'ID', 'SEMICOLON', 'FLOAT', 'ID', 'SEMICOLON', 'RBRACE', 'ID', 'SEMICOLON', 
+        'FLOAT', 'ID', 'LPAREN', 'ID', 'ID', 'COMMA', 'ID', 'ID', 'RPAREN', 'LBRACE', 
+        'FLOAT', 'ID', 'EQUALS', 'ID', 'DOT', 'ID', 'MINUS', 'ID', 'DOT', 'ID', 'SEMICOLON', 
+        'FLOAT', 'ID', 'EQUALS', 'ID', 'DOT', 'ID', 'MINUS', 'ID', 'DOT', 'ID', 'SEMICOLON', 
+        'RETURN', 'ID', 'LPAREN', 'ID', 'TIMES', 'ID', 'PLUS', 'ID', 'TIMES', 'ID', 'RPAREN', 'SEMICOLON', 'RBRACE', 
+        'INT', 'MAIN', 'LPAREN', 'RPAREN', 'LBRACE', 
+        'ID', 'ID', 'EQUALS', 'LBRACE', 'FLOAT_N', 'COMMA', 'FLOAT_N', 'RBRACE', 'SEMICOLON', 
+        'ID', 'ID', 'EQUALS', 'LBRACE', 'FLOAT_N', 'COMMA', 'FLOAT_N', 'RBRACE', 'SEMICOLON', 
+        'FLOAT', 'ID', 'EQUALS', 'ID', 'LPAREN', 'ID', 'COMMA', 'ID', 'RPAREN', 'SEMICOLON', 
+        'ID', 'LPAREN', 'STRING', 'COMMA', 'ID', 'RPAREN', 'SEMICOLON', 
+        'FOR', 'LPAREN', 'INT', 'ID', 'EQUALS', 'INTEGER', 'SEMICOLON', 'ID', 'LT', 'ID', 'SEMICOLON', 'ID', 'PLUS', 'PLUS', 'RPAREN', 'LBRACE', 
+        'IF', 'LPAREN', 'ID', 'MOD', 'INTEGER', 'COMPARATOR', 'INTEGER', 'RPAREN', 'LBRACE', 
+        'ID', 'LPAREN', 'STRING', 'COMMA', 'ID', 'RPAREN', 'SEMICOLON', 'RBRACE', 
+        'ELSE', 'LBRACE', 'ID', 'LPAREN', 'STRING', 'COMMA', 'ID', 'RPAREN', 'SEMICOLON', 'RBRACE', 'RBRACE', 
+        'INT', 'ID', 'EQUALS', 'INTEGER', 'SEMICOLON', 
+        'ID', 'PLUS', 'PLUS', 'SEMICOLON', 
+        'ID', 'MINUS', 'MINUS', 'SEMICOLON', 
+        'RETURN', 'INTEGER', 'SEMICOLON', 'RBRACE'
+    ]
+    
+    tokens = list(lexer.tokenize())
+    
+    # Realizar as verificações
+    for expected_type, token in zip(expected_tokens_types, tokens):
+        assert token.type == expected_type, f"Esperado {expected_type}, obtido {token.type if token else 'Nenhum'}"
+    
+    # Verifica se todos os tokens esperados foram encontrados
+    assert len(tokens) == len(expected_tokens_types), f"Número de tokens esperado {len(expected_tokens_types)}, mas obtido {len(tokens)}"
+    
+    # Após a lista de tokens esperados, não deve haver mais tokens
+    assert lexer.token() is None, "Tokens extras encontrados após os esperados."
